45db7387da0bc5b6b8cd5d80b65240ce46b178d0
Neizmailova Natalia <natata.neizmailova@gmail.com>
2022-12-21 16:28:53 +0200

    Simple lightBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector ('.gallery');
+const galleryCards = createGalleryItem (galleryItems);
+galleryContainer.insertAdjacentHTML(`beforeend`, galleryCards)
 
+function createGalleryItem (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+            />
+        </a>`;
+    })
+    .join("");
+};
 console.log(galleryItems);
+
+new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250
+});
